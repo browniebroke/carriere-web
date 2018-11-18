34c7bd64c8fae6f9f299069f4b0e85b298c3db2f
Bruno Alla <alla.brunoo@gmail.com>
2018-11-18 22:49:47 +0000

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,20 +11,33 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'S.A.R.L Alla',
+        short_name: 'S.A.R.L Alla',
+        description: 'Site du restaurant Chez Nicole',
+        homepage_url: 'https://www.chez-nicole.fr',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        background_color: '#fff',
+        theme_color: '#f7e4bc',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/img/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/img/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     'gatsby-plugin-offline',
   ],
 }
---
+++ src/html.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default class HTML extends React.Component {
+  render() {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          {this.props.headComponents}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/img/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/img/favicon-16x16.png"
+          />
+          <link
+            rel="mask-icon"
+            href="/img/safari-pinned-tab.svg"
+            color="#b2d233"
+          />
+          <link rel="shortcut icon" href="/img/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#b2d233" />
+          <meta name="msapplication-config" content="/img/browserconfig.xml" />
+        </head>
+        <body>
+          <div className="container main-container">
+            <div
+              id="___gatsby"
+              dangerouslySetInnerHTML={{ __html: this.props.body }}
+            />
+          </div>
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+}
