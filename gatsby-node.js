d2b0596f5f3e14a0934b1f4bbf4b6e7e751ed259
Bruno Alla <alla.brunoo@gmail.com>
2018-12-17 17:11:53 +0000

    Attempt to create pages dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode }) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    console.log(fileNode.relativePath)
+  }
+}
