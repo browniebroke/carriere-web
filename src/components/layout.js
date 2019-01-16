fcd49b3363578a726ef3e48b29cabf7a90aa142d
Bruno Alla <alla.brunoo@gmail.com>
2019-01-16 18:34:00 +0000

    Make footer sticky

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,12 @@ const Layout = ({ location, children, containerClass = 'container' }) => (
           ]}
         />
         <Header location={location} containerClass={containerClass} />
-        <div className={containerClass}>{children}</div>
+        <div
+          className={containerClass}
+          style={{ minHeight: 'calc(100vh - 130px - 120px)' }}
+        >
+          {children}
+        </div>
         <Footer containerClass={containerClass} />
       </>
     )}
