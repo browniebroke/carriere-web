b8c5445bed9dfbdff58c0ca8811078bd67b23a3b
Bruno Alla <alla.brunoo@gmail.com>
2019-01-24 21:44:34 +0000

    Not found page in French
    fixes #143

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="row my-5 text-center">
+      <div className="col-md-12">
+        <h1>Page introuvable</h1>
+        <p>
+          Pas moyen de trouver la page recherchée... <br />
+        </p>
+        <p>
+          <Link to="/">Retour à l'acceuil</Link>
+        </p>
+      </div>
+    </div>
   </Layout>
 )
 
