632888ae0822c3d7b985fb9fe973aed28d03504c
Bruno Alla <alla.brunoo@gmail.com>
2019-02-12 20:53:29 +0000

    Move navigation into the main navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import carriereIcon from '../images/logo-full.svg'
 import NavBav from './NavBar'
 import NavItem from './NavItem'
 import PropTypes from 'prop-types'
 
 const Header = ({ location, data, containerClass }) => (
   <header>
-    <div className="navbar-dark bg-primary">
-      <div className={containerClass}>
-        <Link to="/" className="navbar-brand d-flex align-items-center">
-          <img
-            src={carriereIcon}
-            alt=""
-            style={{ width: '200px' }}
-            className="img-fluid mr-3"
-          />
-        </Link>
-      </div>
-    </div>
     <NavBav containerClass={containerClass}>
-      <NavItem location={location} to="/">
-        Acceuil
-      </NavItem>
       <NavItem location={location} to="/photos/">
         Photos
       </NavItem>
