6e4a87675b8c93806f6ce0b8432577776c54192b
Bruno Alla <alla.brunoo@gmail.com>
2019-06-11 19:32:36 +0100

    Contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ContactPage = ({ location, data }) => (
+  <Layout location={location}>
+    <div className="my-5">
+      <h1>Contact</h1>
+    </div>
+  </Layout>
+)
+
+export default ContactPage
