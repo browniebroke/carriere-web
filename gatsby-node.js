3dc350f3255fc7c963b71ea37c73eb021baecffe
Bruno Alla <alla.brunoo@gmail.com>
2019-07-08 18:51:16 +0100

    Rewrite gatsby-node to use async/await instead of Promises

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const path = require(`path`)
 
-// You can delete this file if you're not using it
+// Add the slug field to MarkdownRemark objects
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
@@ -19,42 +19,46 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              frontmatter {
-                images
-                master
-              }
-              fields {
-                slug
-              }
+// Create a page for each markdown file for photos
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  // Run query
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              images
+              master
+            }
+            fields {
+              slug
             }
           }
         }
       }
-    `).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        if (node.fields.slug.startsWith('/photos/')) {
-          createPage({
-            path: node.fields.slug,
-            component: path.resolve(`./src/templates/gallery-page.js`),
-            context: {
-              // Data passed to context is available
-              // in page queries as GraphQL variables.
-              slug: node.fields.slug,
-              imagePath: node.frontmatter.images,
-              masterImage: node.frontmatter.master,
-            },
-          })
-        }
+    }
+  `)
+
+  // In case of errors, stop here
+  if (result.errors) {
+    reporter.panic(`Error creating pages`, reporter.errors)
+  }
+
+  // Otherwise, process result & create pages
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    if (node.fields.slug.startsWith('/photos/')) {
+      actions.createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/gallery-page.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+          imagePath: node.frontmatter.images,
+          masterImage: node.frontmatter.master,
+        },
       })
-      resolve()
-    })
+    }
   })
 }
