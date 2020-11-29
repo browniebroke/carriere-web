4c2568de82c11172d5393276a9071adcbfa4788b
Bruno Alla <browniebroke@users.noreply.github.com>
2020-11-29 20:52:15 +0000

    Small fixes for link & navigation colors

--- src/theme.js
+++ src/theme.js
@@ -4,9 +4,9 @@ const theme = {
   ...styledComponentsTheme,
   colors: {
     primary: '#c71f16',
-    primaryDark: '#80160f',
     secondary: '#e5e5e5',
-    secondaryDark: '#959595',
+    navbar: '#c71f16',
+    navbarText: '#ffffff',
     success: '#28a745',
     info: '#17a2b8',
     warning: '#ffc107',
