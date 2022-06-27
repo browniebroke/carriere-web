5e6f37035d930a10f00e118a2f2a8124779276c6
Bruno Alla <alla.brunoo@gmail.com>
2022-06-27 18:27:22 +0100

    refactor: remove Typography and recreate styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,28 @@
 import { styledComponentsTheme } from '@browniebroke/react-ui-components'
 
-export const theme = {
+export type SiteTheme = {
+  white: string
+  black: string
+  blue: string
+  spacings: string[]
+  mdBreakpoint: string
+  containerMaxWidth: string
+  fontSizes: {
+    base: string
+    h1: string
+    h2: string
+    h3: string
+    h4: string
+    h5: string
+    h6: string
+  }
+  baseFont: string
+  headersFont: string
+}
+
+const baseFontSize = 1.25
+
+export const theme: SiteTheme = {
   ...styledComponentsTheme,
   colors: {
     primary: '#c71f16',
@@ -12,4 +34,15 @@ export const theme = {
     warning: '#ffc107',
     danger: '#dc3545',
   },
+  fontSizes: {
+    base: `${baseFontSize}rem`,
+    h1: `${baseFontSize * 1.7}rem`,
+    h2: `${baseFontSize * 1.3}rem`,
+    h3: `${baseFontSize * 1.15}rem`,
+    h4: `${baseFontSize * 1.1}rem`,
+    h5: `${baseFontSize}rem`,
+    h6: `${baseFontSize * 0.9}rem`,
+  },
+  baseFont: 'Montserrat, "Times New Roman", Times, serif',
+  headersFont: 'Montserrat, Roboto, Arial, sans-serif',
 }
