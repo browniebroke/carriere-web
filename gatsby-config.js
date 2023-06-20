b291571ca46cbf803ab368a0850f54d85642494c
Bruno Alla <alla.brunoo@gmail.com>
2023-06-20 17:01:19 +0100

    Clean up old styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,6 @@ module.exports = {
         isBaseProvider: false,
       },
     },
-    `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
