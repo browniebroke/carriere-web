d02a942a975cfffbee962af7a6da047532eec3d9
Bruno Alla <alla.brunoo@gmail.com>
2025-11-26 18:28:52 +0000

    fix: restore 404 page layout

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
-import { Container, Flex } from '@chakra-ui/react'
+import { Container, Flex, Heading, Text } from '@chakra-ui/react'
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Page introuvable</h1>
-      <p>Cette page n&apos;existe pas...</p>
-      <Link href="/">Retour à l&apos;acceuil</Link>
-    </div>
+    <Container maxWidth="4xl">
+      <Flex
+        minHeight="60vh"
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        marginTop={10}
+        marginBottom={10}
+      >
+        <Heading as="h1">Page introuvable</Heading>
+        <Text>
+          Cette page n&apos;existe pas... <br />
+        </Text>
+        <Text>
+          <Link href="/">Retour à l&apos;acceuil</Link>
+        </Text>
+      </Flex>
+    </Container>
   )
 }
